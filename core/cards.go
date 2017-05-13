dd113baf2060240b6a425e114a7b16b193db762f
MaximeEngel <maximeengel@gmail.com>
2017-05-13 17:48:18 +0200

    Rename deck type as cards and add draw method

---
+++ core/cards.go
@@ -0,0 +1,47 @@
+package core
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+const NB_CHARS = 45
+const NB_EVENTS = 10
+const NB_CARDS = NB_CHARS + NB_EVENTS
+
+type cards []*card
+
+func Deck() *cards {
+	d := make(cards, 0, NB_CARDS)
+	for i := 1 ; i < 10 ; i++ {
+		for j := 0 ; j < i; j++ {
+			d = append(d, &card{Influence:i})
+		}
+	}
+	return &d
+}
+
+func (cardsPtr *cards) Shuffle() {
+	c := *cardsPtr
+	for i := range c{
+		j := rand.Intn(i + 1)
+		c[i], c[j] = c[j], c[i]
+	}
+}
+
+func (cardsPtr *cards) Draw() *card {
+	c := *cardsPtr
+	drawed_card := c[len(c) - 1]
+	c[len(c) -1 ] = nil
+	*cardsPtr = c[:len(c) - 1]
+	return drawed_card
+}
+
+func (cardsPtr *cards) String() (s string) {
+	s = ""
+	for _, v := range *cardsPtr {
+		s += fmt.Sprintf("%v", *v)
+	}
+	return
+}
+
