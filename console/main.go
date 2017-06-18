3279e498a9abd3980ff9861ed83c55cd91f34c03
MaximeEngel <maximeengel@gmail.com>
2017-06-18 12:39:38 +0200

    Add [Next](First|Current)Player() methods

--- console/main.go
+++ console/main.go
@@ -9,20 +9,9 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	deck := core.Deck()
-	deck.Shuffle()
-	fmt.Println(deck)
-	player1 := core.Player("Maxime")
-	for i := 0; i < 8; i++ {
-		player1.Draw(deck.Draw())
+	gb := core.GameBoard([]string{"Maxime", "Eloise", "Nathalie"})
+
+	for player := gb.FirstPlayer(); player != nil ; player = gb.NextCurrentPlayer() {
+		fmt.Println(player)
 	}
-	fmt.Println(deck)
-	fmt.Println(player1)
-	hand := player1.HandConst()
-	for i := 0; i < 4; i++ {
-		player1.SelectPlayCardPtr(hand[i])
-	}
-	player1.PlayCard(0)
-	player1.PlayCard(1)
-	fmt.Println(player1)
 }
--- core/gameboard.go
+++ core/gameboard.go
@@ -7,6 +7,7 @@ import (
 type gameBoard struct {
 	players []*player
 	first_player_idx int
+	current_player_idx int
 	deck *cards
 	season int
 }
@@ -18,10 +19,44 @@ func GameBoard(players_name []string) *gameBoard {
 	}
 	deck := Deck()
 	deck.Shuffle()
+	first_player := rand.Intn(len(players))
 
 	return &gameBoard{
 		players:players,
-		first_player_idx:rand.Intn(len(players)),
+		first_player_idx: first_player,
+		current_player_idx: first_player,
 		deck:deck,
 		season:1}
+}
+
+func (gb *gameBoard) FirstPlayer() *player {
+	return gb.players[gb.first_player_idx]
+}
+
+func (gb *gameBoard) NextFirstPlayer() *player {
+	next_idx := gb.first_player_idx + 1
+	if next_idx >= len(gb.players) {
+		next_idx = 0
+	}
+
+	gb.first_player_idx = next_idx
+	return gb.FirstPlayer()
+}
+
+func (gb *gameBoard) CurrentPlayer() *player {
+	return gb.players[gb.current_player_idx]
+}
+
+func (gb* gameBoard) NextCurrentPlayer() *player {
+	next_idx := gb.current_player_idx + 1
+	if next_idx >= len(gb.players) {
+		next_idx = 0
+	}
+
+	if next_idx == gb.first_player_idx {
+		return nil
+	}
+
+	gb.current_player_idx = next_idx
+	return gb.CurrentPlayer()
 }
